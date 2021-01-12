7c077a8e5d8920aa95983109e4e0449c16a48bfc
secundant <mysecundant@gmail.com>
2021-01-12 12:29:26 +0300

    chore: Sync files

--- packages/react-nextjs/pages/_error.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NextErrorPage, { ErrorProps } from 'next/error';
-
-export default function MyErrorPage(props: ErrorProps) {
-  return <NextErrorPage {...props} />;
-}
